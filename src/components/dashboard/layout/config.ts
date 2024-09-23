c10980de8fca99ccde1261f94455f4dc50c1a912
RenanGeorgio <regeo9@hotmail.com>
2024-09-23 12:21:38 -0300

    up

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -5,6 +5,4 @@ export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
   { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
-  { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
 ] satisfies NavItemConfig[];
