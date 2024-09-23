319e78029c3d97d5f89b1e7fd0882eeb983a58a4
RenanGeorgio <regeo9@hotmail.com>
2024-09-23 12:39:48 -0300

    up

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -46,18 +46,18 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
             <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
               Welcome to{' '}
               <Box component="span" sx={{ color: '#15b79e' }}>
-                Devias Kit
+                Diamondbigger Plataform User
               </Box>
             </Typography>
             <Typography align="center" variant="subtitle1">
-              A professional template that comes with ready-to-use MUI components.
+              A professional plataform ready-to-use.
             </Typography>
           </Stack>
           <Box sx={{ display: 'flex', justifyContent: 'center' }}>
             <Box
               component="img"
               alt="Widgets"
-              src="/assets/auth-widgets.png"
+              src="/assets/auth-diamondbigger.png"
               sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
             />
           </Box>
